225ef1b6b3503e5053bd4e24c6a20e5fcf63c109
Mahi <maheshmorde2511@gmail.com>
2023-12-03 20:30:00 +0530

    Dec 3 upload

---
+++ Dec_3_2023.py
@@ -0,0 +1,17 @@
+# 
+
+
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        s=0
+        for i in range(len(points)-1):
+            a=points[i]
+            b=points[i+1]
+            max1=max(a[0],b[0])
+            min1=min(a[0],b[0])
+            max2=max(a[1],b[1])
+            min2=min(a[1],b[1])
+            c=max((max1-min1),(max2-min2))  
+            s+=c 
+        return (s)
